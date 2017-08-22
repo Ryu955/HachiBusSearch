b0cfc2496b64d3baacee85a4a69229fd2c922a26
Ryu955 <c0114046aa@edu.teu.ac.jp>
2017-08-22 21:45:40 +0900

    Add scraping TrainTimeTable file

--- getTrainTimeTable.py
+++ getTrainTimeTable.py
@@ -3,14 +3,14 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 #URLの指定
-html = urlopen("http://www.teu.ac.jp/campus/access/2017_kihon-a_bus.html")
+html = urlopen("http://timetable.ekitan.com/train/TimeStation/25-0_D1.shtml")
 bsObj = BeautifulSoup(html, "html.parser")
 
 #テーブルを指定
-table = bsObj.findAll("table")[1]
+table = bsObj.findAll("table")[0]
 rows = table.findAll("tr")
 
-csvFile = open("BusTimeTable.csv", 'wt', newline = '', encoding = 'utf-8')
+csvFile = open("TrainTimeTable.csv", 'wt', newline = '', encoding = 'utf-8')
 writer = csv.writer(csvFile)
 
 try:
@@ -18,9 +18,6 @@ try:
         csvRow = []
         for cell in row.findAll(['td', 'th']):
             csvRow.append(cell.get_text())
-        if csvRow == "['～', '～', '～', 'シャトル運行（約5～10分間隔）']":
-            writer.writerow("シャトル運行")
-        else:
-            writer.writerow(csvRow)
+        writer.writerow(csvRow)
 finally:
     csvFile.close()
