c040ff9a06c317576b0d7000a2b0ce8d0067ad50
Ryu955 <c0114046aa@edu.teu.ac.jp>
2017-08-23 02:51:45 +0900

    Add TabViewController and webview

---
+++ HachiBusTimeTable/HachiBusTimeTable/WebViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  HachiBusTimeTable
+//
+//  Created by Ryuta Ishikawa on 2017/08/22.
+//  Copyright © 2017年 Ryuta Ishikawa. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+    
+    @IBOutlet weak var webview: UIWebView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        // urlとかをセットして　表示するだけ
+        if let url = URL(string: "http://www.teu.ac.jp/campus/access/006644.html"){
+            let request = URLRequest(url: url)
+            webview.loadRequest(request)
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
