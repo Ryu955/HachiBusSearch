4a6ea5c67e516d4b0d1f778de015b5e2bc8724eb
Ryu955 <c0114046aa@edu.teu.ac.jp>
2017-09-04 05:05:40 +0900

    Add ReturnBusTimeTableView
    帰りの時刻表を追加した

--- HachiBusTimeTable/HachiBusTimeTable/ReturnBusTimeTableView.swift
+++ HachiBusTimeTable/HachiBusTimeTable/ReturnBusTimeTableView.swift
@@ -1,20 +1,21 @@
 import UIKit
 
-class GoingBusTimeTableView: UIViewController , UITableViewDataSource, UITableViewDelegate{
+class ReturnBusTimeTableView: UIViewController , UITableViewDataSource, UITableViewDelegate{
     
-    @IBOutlet var table:UITableView!
+  //  @IBOutlet var table:UITableView!
+    @IBOutlet var table: UITableView!
     
     // 配列
     private var timeTable:[String] = []
-
-    private var formStation:[String] = []
-    private var arriveCampus:[String] = []
+    
+    private var arriveStation:[String] = []
+    private var formCampus:[String] = []
     
     var count = 0
     
     // Sectionのタイトル
     let sectionTitle: NSArray = ["出発: 到着"]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,22 +25,22 @@ class GoingBusTimeTableView: UIViewController , UITableViewDataSource, UITableVi
     
     //Table Viewのセルの数を指定
     func tableView(_ table: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return formStation.count
+        return arriveStation.count
     }
     
     //各セルの要素を設定する
     func tableView(_ table: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         // tableCell の ID で UITableViewCell のインスタンスを生成
-        let cell = table.dequeueReusableCell(withIdentifier: "goingBusTimetableCell", for: indexPath)
+        let cell = table.dequeueReusableCell(withIdentifier: "returnBusTimetableCell", for: indexPath)
         
         // Tag番号 1 で UILabel インスタンスの生成
         let label1 = table.viewWithTag(1) as! UILabel
-        label1.text = "\(formStation[indexPath.row])"
+        label1.text = "\(formCampus[indexPath.row])"
         
         // Tag番号 2 で UILabel インスタンスの生成
         let label2 = table.viewWithTag(2) as! UILabel
-        label2.text = "\(arriveCampus[indexPath.row])"
+        label2.text = "\(arriveStation[indexPath.row])"
         
         
         return cell
@@ -68,32 +69,30 @@ class GoingBusTimeTableView: UIViewController , UITableViewDataSource, UITableVi
             }
         }
     }
-
+    
     func timeTableFormating(){
         for time in timeTable {
             
             if(time == "シャトル運行（約5～10分間隔）" || time == "シャトル運行（約3～5分間隔）"){
-                formStation.append(time)
-                arriveCampus.append(time)
+                arriveStation.append(time)
+                formCampus.append(time)
                 count = 0
                 continue
             }
             
             if(count == 0){
+                formCampus.append(time)
                 count += 1
             }else if(count == 1){
-                formStation.append(time)
+                arriveStation.append(time)
                 count += 1
             }else{
-                arriveCampus.append(time)
                 count = 0
             }
-            
-            
         }
         
-        print(formStation)
-        print(arriveCampus)
+        print(arriveStation)
+        print(formCampus)
         
     }
     
@@ -101,6 +100,6 @@ class GoingBusTimeTableView: UIViewController , UITableViewDataSource, UITableVi
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     
 }
