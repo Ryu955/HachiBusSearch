c90bde75a6f801f9a5612a53c387248336d3c680
Ryu955 <c0114046aa@edu.teu.ac.jp>
2017-09-06 07:01:28 +0900

    Update UI approximately completion

---
+++ HachiBusTimeTable/HachiBusTimeTable/ResultViewController.swift
@@ -0,0 +1,22 @@
+
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
--- HachiBusTimeTable/HachiBusTimeTable/ViewController.swift
+++ HachiBusTimeTable/HachiBusTimeTable/ViewController.swift
@@ -3,7 +3,7 @@ import CoreActionSheetPicker
 
 class ViewController: UIViewController, UIPickerViewDelegate  {
     
-    @IBOutlet weak var rideTime: UILabel!
+    @IBOutlet weak var selectButton: UIButton!
     
     @IBAction func selectTime(_ sender: Any) {
         SelectTime()
@@ -11,47 +11,26 @@ class ViewController: UIViewController, UIPickerViewDelegate  {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+         let now = Date()
+        
+        
+    
         
     }
-    func actionSheet(){
-        ActionSheetMultipleStringPicker.show(withTitle: "Multiple String Picker", rows: [
-            [1, 2, 3],
-            [00, 01, 02]
-            ], initialSelection: [2, 2], doneBlock: {
-                picker, indexes, values in
-                
-                self.rideTime.text = "\(values) , \(indexes) , \(picker)"
-                print("values = \(values)")
-                print("indexes = \(indexes)")
-                print("picker = \(picker)")
-                return
-        }, cancel: { ActionMultipleStringCancelBlock in return }, origin: send)
-    }
 
-//    func timeSheet(){
-//        let timePicker = ActionSheetDatePicker(title: "Date:", datePickerMode: UIDatePickerMode.date, selectedDate: Date(), doneBlock: {
-//            picker, value, index in
-//            dateSelected.text = formatter.string(for: time)
-//            print("value = \(value)")
-//            print("index = \(index)")
-//            print("picker = \(picker)")
-//            return
-//        }, cancel: { ActionStringCancelBlock in
-//            print("cancel")
-//            return }, origin: sender.superview)
-//        timePicker?.minuteInterval = 5
-//        timePicker?.locale = Calendar.current.locale
-//    }
 
     
     func SelectTime(){
-        let timePicker = ActionSheetDatePicker(title: "Date:", datePickerMode: UIDatePickerMode.time, selectedDate: Date(), doneBlock: {
+        let timePicker = ActionSheetDatePicker(title: "帰宅時間", datePickerMode: UIDatePickerMode.time, selectedDate: Date(), doneBlock: {
             picker, value, index in
             
             let formatter = DateFormatter()
             formatter.dateFormat = "HH:mm"
-            self.rideTime.text = formatter.string(for: value)
+            
+            self.selectButton.titleLabel?.text = formatter.string(for: value)! + "に帰る"
+            
             print("value = \(value)")
             print("index = \(index)")
             print("picker = \(picker)")
@@ -62,7 +41,6 @@ class ViewController: UIViewController, UIPickerViewDelegate  {
         timePicker?.minuteInterval = 5
         timePicker?.locale = Calendar.current.locale
         timePicker?.show()
-        
         
     }
 }
