f0aa1a701a4699b8a32e20df59d797589bd96e09
Ryu955 <c0114046aa@edu.teu.ac.jp>
2017-09-06 01:53:28 +0900

    Update selectTime
    時間選択をactionsheetPickerにした
    選択した文字列を受け取れる様になった

--- HachiBusTimeTable/HachiBusTimeTable/ViewController.swift
+++ HachiBusTimeTable/HachiBusTimeTable/ViewController.swift
@@ -1,11 +1,33 @@
 import UIKit
+import CoreActionSheetPicker
 
 class ViewController: UIViewController, UIPickerViewDelegate  {
     
+    @IBOutlet weak var rideTime: UILabel!
     
+    @IBAction func selectTime(_ sender: Any) {
+        actionSheet()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        
     }
+    func actionSheet(){
+        ActionSheetMultipleStringPicker.show(withTitle: "Multiple String Picker", rows: [
+            ["One", "Two", "A lot"],
+            ["Many", "Many more", "Infinite"]
+            ], initialSelection: [2, 2], doneBlock: {
+                picker, indexes, values in
+                
+                self.rideTime.text = "\(values) , \(indexes) , \(picker)"
+                print("values = \(values)")
+                print("indexes = \(indexes)")
+                print("picker = \(picker)")
+                return
+        }, cancel: { ActionMultipleStringCancelBlock in return }, origin: send)
+    }
+
     
 }
