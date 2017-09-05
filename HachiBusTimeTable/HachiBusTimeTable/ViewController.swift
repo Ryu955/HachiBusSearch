cd698f759862d23993d8fbd83acd415b78e7c252
Ryu955 <c0114046aa@edu.teu.ac.jp>
2017-09-06 03:24:22 +0900

    Update selectTime
    時間を選択出来るようにした

--- HachiBusTimeTable/HachiBusTimeTable/ViewController.swift
+++ HachiBusTimeTable/HachiBusTimeTable/ViewController.swift
@@ -6,18 +6,18 @@ class ViewController: UIViewController, UIPickerViewDelegate  {
     @IBOutlet weak var rideTime: UILabel!
     
     @IBAction func selectTime(_ sender: Any) {
-        actionSheet()
+        SelectTime()
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+
         
     }
     func actionSheet(){
         ActionSheetMultipleStringPicker.show(withTitle: "Multiple String Picker", rows: [
-            ["One", "Two", "A lot"],
-            ["Many", "Many more", "Infinite"]
+            [1, 2, 3],
+            [00, 01, 02]
             ], initialSelection: [2, 2], doneBlock: {
                 picker, indexes, values in
                 
@@ -29,5 +29,40 @@ class ViewController: UIViewController, UIPickerViewDelegate  {
         }, cancel: { ActionMultipleStringCancelBlock in return }, origin: send)
     }
 
+//    func timeSheet(){
+//        let timePicker = ActionSheetDatePicker(title: "Date:", datePickerMode: UIDatePickerMode.date, selectedDate: Date(), doneBlock: {
+//            picker, value, index in
+//            dateSelected.text = formatter.string(for: time)
+//            print("value = \(value)")
+//            print("index = \(index)")
+//            print("picker = \(picker)")
+//            return
+//        }, cancel: { ActionStringCancelBlock in
+//            print("cancel")
+//            return }, origin: sender.superview)
+//        timePicker?.minuteInterval = 5
+//        timePicker?.locale = Calendar.current.locale
+//    }
+
     
+    func SelectTime(){
+        let timePicker = ActionSheetDatePicker(title: "Date:", datePickerMode: UIDatePickerMode.time, selectedDate: Date(), doneBlock: {
+            picker, value, index in
+            
+            let formatter = DateFormatter()
+            formatter.dateFormat = "HH:mm"
+            self.rideTime.text = formatter.string(for: value)
+            print("value = \(value)")
+            print("index = \(index)")
+            print("picker = \(picker)")
+            return
+        }, cancel: { ActionStringCancelBlock in
+            print("cancel")
+            return }, origin: self.view)
+        timePicker?.minuteInterval = 5
+        timePicker?.locale = Calendar.current.locale
+        timePicker?.show()
+        
+        
+    }
 }
