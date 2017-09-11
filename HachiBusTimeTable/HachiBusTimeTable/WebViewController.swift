02989704cd8ad74b50c6c3527104c34c0d620717
Ryu955 <c0114046aa@edu.teu.ac.jp>
2017-09-11 16:40:41 +0900

    Update webView to WKWebView

--- HachiBusTimeTable/HachiBusTimeTable/WebViewController.swift
+++ HachiBusTimeTable/HachiBusTimeTable/WebViewController.swift
@@ -7,33 +7,57 @@
 //
 
 import UIKit
+import WebKit
 
-class WebViewController: UIViewController {
+class WebViewController: UIViewController ,WKNavigationDelegate{
     
-    @IBOutlet weak var webview: UIWebView!
+   // @IBOutlet weak var webview: UIWebView!
     
     
 
+    @IBOutlet weak var contentView: UIView!
     
+    private var webView: WKWebView!
     var selectedURL: URL?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // urlとかをセットして　表示するだけ
         if let url = selectedURL{
             let request = URLRequest(url: url)
-            webview.loadRequest(request)
+            
+            
+            //WkWebView 生成
+            self.webView = WKWebView(frame: CGRect.zero)
+            
+            //デリケート設定
+            self.webView.navigationDelegate = self
+            
+            //フリップでの戻る・進むを有効にする
+            self.webView.allowsBackForwardNavigationGestures = true
+            self.contentView.addSubview(self.webView)
+            
+            //ページ読み込み
+            //            let url = NSURL(string: "https://www.google.co.jp/")
+            //            let urlRequest = URLRequest(url: url as! URL)
+            self.webView.load(request)
         }
-        
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidLayoutSubviews() {
+        //WKWebView リサイズ
+        self.webView.frame = CGRect(origin: CGPoint.zero, size: self.contentView.frame.size)
     }
     
-  
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        // ネットワークインジケータを表示
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true;
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        // ネットワークインジケータを非表示
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false;
+    }
     
 
 }
